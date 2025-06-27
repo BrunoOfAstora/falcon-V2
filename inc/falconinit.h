b71537f3d3c324c17e7d6999353acfef57ee5f09
BrunoOfAstora <bruno.cntt@hotmail.com>
2025-06-26 21:02:47 -0300

    [UPDATE]:Adding main funtion. Now the program works again!

--- inc/falconinit.h
+++ inc/falconinit.h
@@ -1,6 +1,7 @@
 #ifndef FALCONINIT_H
 #define FALCONINIT_H
 
+#include <getopt.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -13,25 +14,35 @@
 typedef struct FalconHashInit
 {
 
-	FILE *file_usr_stream;
-	size_t buf_hash_size;
-	size_t bytes_read;
+	FILE *file_usr_stream; //---------------------------->Used to open the user file
+	
+	size_t buf_hash_size;//------------------------------>Store the size of the hash buffer
 
-	uint8_t file_hash_buf_s[FILE_HASH_BUF_SIZE];
-	unsigned char md_hash_buf[EVP_MAX_MD_SIZE];
-	unsigned int hash_md_leng;	
+	size_t bytes_read;//--------------------------------->Quantity of bytes readed from the stream
 
-	char *hex_hash_result;
+	uint8_t file_hash_buf_s[FILE_HASH_BUF_SIZE];//------->Size of buffer to store the hash (64) -> *Openssl Documentation
+	
+	unsigned char md_hash_buf[EVP_MAX_MD_SIZE];//-------->Actual buffer that contains chuncks of hash
+	
+	unsigned int hash_md_leng;//------------------------->Defines the lenght of hash to write on	
+
+	char *hex_hash_result;//----------------------------->Stores the result of given hash
 	
 }flcn_init;
 
-
+//Returns the MD5 hash of the file passed
 char *flcn_md5_hash(const char *usr_in);
 
+
+//Returns the SHA256 hash of the file passed
 char *flcn_256_hash(const char *usr_in);
 
+
+//Returns the SHA384 hash of the file passed
 char *flcn_384_hash(const char *usr_in);
 
+
+//Returns the SHA512 hash of the file passed
 char *flcn_512_hash(const char *usr_in);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,4 @@
 #include "falconinit.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <getopt.h>
-
 
 int main(int argc, char *argv[])
 {
@@ -11,22 +6,104 @@ int main(int argc, char *argv[])
 
 	if(argc < 2)
 	{
-		printf("\nLess than one argument, use 'falcon -h'\n");
 		goto end;
 	}
 	
-	
-	const char *md5test = flcn_512_hash(argv[1]);
-	printf("MD5 = %s\n",md5test);
+	int opt,
+		opt_index;
 
+	while(1)
+	{
+	//	int opt_optind = optind ? optind : 1;  Unused for now;
+		static struct option long_options[] = 
+		{
+			{"md5", 	required_argument, 	0, 'm'},
+			{"sha256", 	required_argument, 	0, '2'},
+			{"sha384", 	required_argument,	0, '3'},
+			{"sha512", 	required_argument,	0, '5'},
+			{"help", 	no_argument,		0, 'h'},
+			{0, 		0, 					0,  0 },
+		};
+
+		opt = getopt_long(argc, argv, "m:2:3:5:h", long_options, &opt_index);
+		if(opt == -1)
+				break;
+
+		switch(opt)
+		{
+			case 0:
+				printf("options %s, ", long_options[opt_index].name);
+				if(optarg)
+					printf(" arguments %s", optarg);
+				break;
+
+			case 'm':
+				if(optarg[0] == '-')
+				{
+					fprintf(stderr, "Error: md5 option require a file as argument, but got '%s' as argument instead\n", optarg);
+						exit(EXIT_FAILURE);
+				}
+				
+				char *md5_print = flcn_md5_hash(argv[2]);
+				printf("MD5: %s\n", md5_print);	
+				break;
+
+
+			case '2':
+				if(optarg[0] == '-')
+				{
+					fprintf(stderr, "Error: sha256 option require a file as argument, but got '%s' as argument instead\n", optarg);
+						exit(EXIT_FAILURE);
+				}
+			
+				char *sha256_print = flcn_256_hash(argv[2]);
+				printf("SHA256: %s\n", sha256_print);
+				break;
+
+
+			case '3':
+				if(optarg[0] == '-')
+				{
+					fprintf(stderr, "Error: sha384 option require a file as argument, but got '%s' as argument instead\n", optarg);
+						exit(EXIT_FAILURE);
+				}
+
+				char *sha384_print = flcn_384_hash(argv[2]);
+				printf("SHA384: %s\n", sha384_print);
+				break;
+
+
+			case '5':
+				if(optarg[0] == '-')
+				{
+					fprintf(stderr, "Error: sha512 option require a file as argument, but got '%s' as argument instead\n", optarg);
+						exit(EXIT_FAILURE);
+				}
+
+				char *sha512_print = flcn_512_hash(argv[2]);
+				printf("SHA512: %s\n", sha512_print);
+				break;
+
+			case 'h':
+				printf("Help\n	Options =>\n");
+				break;
+
+			default:
+				printf("\nType 'falcon -h' or 'falcon --help' to see how to use falcon\n");
+		}
+
+	}
+	if(optind < argc)
+	{
+		printf("No options for: ");	
+		while(optind < argc)
+			printf("%s ", argv[optind++]);
+		printf("\n");
+	}
+	
+	exit(EXIT_SUCCESS);
 	ret_num = 0;
 
-end:
+end: //maybe useless for now
 	return ret_num;
-
-
-place_holder:
-	printf("\nPlace Holder Function Example\n");
-	return 10;
-
 }
