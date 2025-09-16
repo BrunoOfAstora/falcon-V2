b6554b78acf782243a0fe30039f88dc1250bfbd5
BrunoOfAstora <bruno.cntt@hotmail.com>
2025-09-16 18:19:30 -0300

    [REFACTOR]verify function (dependecies)

---
+++ inc/flcn_verify.h
@@ -0,0 +1,41 @@
+#ifndef FALCON_V2_FLCN_VERIFY_H
+#define FALCON_V2_FLCN_VERIFY_H
+
+#include <sqlite3.h>
+#include <openssl/crypto.h>
+
+#define FLCN_SQL_REQUEST "SELECT f_name, f_hash FROM flcn_hashes WHERE f_name = ?"
+
+typedef enum
+{
+    FLCN_DB_ROW,
+    FLCN_DB_DONE,
+    FLCN_DB_ERROR
+};
+
+typedef struct CmpHandler
+{
+    char *file_hash;
+    char *file_name;
+}CmpHandler;
+char *flcn_verify_get_file_path(const char *file_name);
+
+int flcn_verify_init_db(sqlite3 **db, const char *db_path);
+
+int flcn_verify_prepare_db(sqlite3 *db, const char *request, sqlite3_stmt **stmt);
+
+int flcn_verify_db_bind_text(sqlite3_stmt *stmt, const char *file_name);
+
+int flcn_verify_step_db(sqlite3_stmt *stmt);
+
+void flcn_verify_finalize_db_stmt(sqlite3_stmt *stmt);
+
+void flcn_verify_close_db(sqlite3 *db);
+
+char *flcn_verify_column_strdup(sqlite3_stmt *stmt, int column);
+
+int flcn_verify_crypto_cmp(char *str1, char *str2, size_t len);
+
+int flcn_verify_digest(CmpHandler *cmp_handler, sqlite3 *db, sqlite3_stmt *stmt, const char *db_path,char *file_name);
+
+#endif //FALCON_V2_FLCN_VERIFY_H
--- src/flcn-verify.c
+++ src/flcn-verify.c
@@ -1,160 +1,65 @@
 #include "falconinit.h"
-#include <sqlite3.h>
-#include <openssl/crypto.h>
-
 #include "flcn-hashes.h"
+#include "flcn_verify.h";
 
-typedef struct Compare_att
-{
-	char *hash;
-	char *f_name;
-
-}cmp_info;
-
-unsigned int f_err = 0;
-
-int verify_callback(const char *f_path, const struct stat *st, int flag, struct FTW *ftwbuf)
-{
-	(void)st;    
-    (void)ftwbuf; 	
-
-	if(flag == FTW_F)
-		flcn_verify((char *)f_path);
-
-	/*else if (flag == FTW_D || flag == FTW_DP)
-	{
-	//	if(ftwbuf->level > 1)
-			return FTW_SKIP_SUBTREE;
-	}*/
-	
-	return 0;
-}
-
-
-int flcn_verify_all(const char *start_path)
+/*int flcn_verify_get_pat(const char *path)
 {
 	struct stat st;
-	
-	if(stat(start_path, &st) == -1)
-	{
-		perror("Start Failed:");
-		return 1;
-	}
-
-	if(S_ISDIR(st.st_mode))
-	{
-		nftw(start_path, verify_callback, 1, FTW_PHYS);
-		printf("\n_______________________________\n");
-		fflush(stdout);
-		printf("Divergences found: %d", f_err);
-		printf("\n_______________________________\n");
-		fflush(stdout);
-		return 0;
-	}
-
-	else if (S_ISREG(st.st_mode))
-		return flcn_verify((char *)start_path);
-
-	else
-	{
-		printf("Unsupported file type\n");
-		return 1;
-	}
+	return stat(path, &st);
 }
-
-
-int flcn_verify(char *f_name)
+*/
+char *flcn_verify_get_file_path(const char *file_name)
 {
-	sqlite3 *db;
-	sqlite3_stmt *stmt;
-	cmp_info cmp;
-
-	bool f_name_flag = false;
-	bool f_hash_flag = false;
-
-
-	if(f_name == NULL)
-	{
-		fprintf(stderr,"Error while opening file\n");
-		return 1;
-	}
-
-	char *abs_f_name = realpath(f_name, NULL);
-    if (abs_f_name == NULL) {
-        perror("Error getting realpath");
-        return 1;
-    }
-
-	const char *sql_request = "SELECT f_name, f_hash FROM flcn_hashes WHERE f_name = ?";
-	
-	if((sqlite3_open_v2(flcn_save_init->full_db_path, &db, SQLITE_OPEN_READWRITE, NULL)) != SQLITE_OK)
-	{
-		fprintf(stderr, "SQLite error: Failed opening DB for verify: %s\n", sqlite3_errmsg(db));
-		return 1;
-	}
-
-	if((sqlite3_prepare_v2(db, sql_request, -1, &stmt, NULL) != SQLITE_OK))
-	{
-		fprintf(stderr, "SQLite error: Error while preparing SQL statement: %s\n", sqlite3_errmsg(db));
-		return 1;
-	}
-
-	const char *f_hash = flcn_build_hash(f_name, EVP_sha256());
-	if(f_hash == NULL)
-	{
-		printf("Error opening file to calculate Hash\n");
-		return 1;
-	}
-
-	sqlite3_bind_text(stmt, 1, abs_f_name, -1, SQLITE_STATIC);
-
-	if (sqlite3_step(stmt) == SQLITE_ROW)
-	{
-		cmp.f_name = strdup((const char *)sqlite3_column_text(stmt, 0));
-		cmp.hash   = strdup((const char *)sqlite3_column_text(stmt, 1));
-
-		size_t leng1 = strlen(cmp.f_name);
-		size_t leng2 = strlen(abs_f_name);
-
-		if(sec_memcmp(cmp.f_name, abs_f_name, leng2) == true && leng1 == leng2) 
-			f_name_flag = true;
-	
-		if(sec_memcmp(cmp.hash, f_hash, 32) == true)
-			f_hash_flag = true;
-		
-		free(cmp.f_name);
-		free(cmp.hash);
-	}
-
-	printf("\n");
-	printf("Checking %s...", basename(f_name));
-	if(f_name_flag == true)
-		printf("\033[32mOK!\033[0m\n");
-	else
-		printf("\033[31mFailed\033[0m\n");
-
-
-	printf("Checking %s hash...", basename(f_name));
-	if(f_hash_flag)
-		printf("File Hash: \033[32mOK!\033[0m\n");
-	else if(!f_name_flag && f_hash_flag)
-		printf("\033[32mOK\033[0m\n\033[33mNotice\033[0m: This file was found in database but with a different name\n");
-	else
-	{
-		f_err++;
-		printf("\033[31mFailed\033[0m\n");
-	}
-
-	if(!f_name_flag && !f_hash_flag)
-		printf("File not found in DB\n");
-
-	if(f_name_flag && !f_hash_flag)
-		printf("\033[31mFailed\033[0m: A file with name %s was found in database, but the hash does not match, the file may be different or corrupted\n", basename(f_name));
-
-	sqlite3_finalize(stmt);
-
-	if(db)
-		sqlite3_close(db);
-
-	return 0;
+	char *rp = realpath(file_name, NULL);
+	if (rp == NULL)
+		return NULL;
+	return rp;
 }
+
+// SQL Operations
+int flcn_verify_init_db(sqlite3 **db, const char *db_path)
+{
+	return sqlite3_open_v2(db_path, db, SQLITE_OPEN_READWRITE, NULL);
+
+}
+
+int flcn_verify_prepare_db(sqlite3 *db, const char *request, sqlite3_stmt **stmt)
+{
+	return sqlite3_prepare_v2(db, request, -1, stmt, NULL);
+}
+
+int flcn_verify_db_bind_text(sqlite3_stmt *stmt, const char *file_name)
+{
+	return sqlite3_bind_text(stmt, 1, file_name, -1, SQLITE_STATIC);
+}
+
+int flcn_verify_step_db(sqlite3_stmt *stmt)
+{
+	int step = sqlite3_step(stmt);//is this easier to read? maybe?
+	if (step == SQLITE_ROW) return FLCN_DB_ROW;
+	if (step == SQLITE_DONE) return FLCN_DB_DONE;
+	if (step == SQLITE_ERROR) return FLCN_DB_ERROR;
+}
+
+void flcn_verify_finalize_db_stmt(sqlite3_stmt *stmt)
+{
+	sqlite3_finalize(stmt);
+}
+
+void flcn_verify_close_db(sqlite3 *db)
+{
+	sqlite3_close(db);
+}
+
+char *flcn_verify_column_strdup(sqlite3_stmt *stmt, int column)
+{
+	return strdup((const char *)sqlite3_column_text(stmt, column));
+}
+// END SQL Operations
+//
+
+int flcn_verify_crypto_cmp(char *str1, char *str2, size_t len)
+{
+	return CRYPTO_memcmp(str1, str2, len);
+}
+//
